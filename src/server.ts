7b524193e24f960a06d6d52cf468f7ce9b87470e
jahidhasan-webdev01 <jahidhasan.webdev01@gmail.com>
2026-07-15 18:10:26 +0600

    initial backend setup

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL!);
+
+        console.log("MongoDB Connected");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
